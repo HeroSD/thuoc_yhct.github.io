a9bb736296e5dfc07353dc3e4f57a9fc75432b38
HeroXD <tncuongdev2022@gmail.com>
2026-07-17 00:59:44 +0700

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// script.js
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTopBtn.classList.add("show");
+    } else {
+        backToTopBtn.classList.remove("show");
+    }
+});
